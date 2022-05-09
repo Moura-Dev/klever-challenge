43d29dfcd5fd3f41d2dfd8f4e89e7b094be69bb9
moura-dev <junior.moura19@hotmail.com>
2022-05-09 15:28:42 -0300

    Implements all methods

--- controllers/coinController.go
+++ controllers/coinController.go
@@ -10,6 +10,9 @@ import (
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	bson "go.mongodb.org/mongo-driver/bson/primitive"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type UpvoteServiceServer struct {
@@ -18,10 +21,36 @@ type UpvoteServiceServer struct {
 }
 
 func (s *UpvoteServiceServer) ListCoins(req *pb.ListCoinsRequest, stream pb.UpvoteService_ListCoinsServer) error {
-	coins, err := s.CoinsRepository.GetAll()
+	cursor, err := s.CoinsRepository.GetAll()
+	if err != nil {
+		return err
+	}
+	defer cursor.Close(context.Background())
 
-	fmt.Println(coins, err)
+	var coin models.Coin
+	for cursor.Next(context.Background()) {
+		err := cursor.Decode(&coin)
+		if err != nil {
+			return status.Errorf(codes.Unavailable, fmt.Sprintf("Could not decode data: %v", err))
+		}
+
+		stream.Send(coin.ToProtoBufferListCoin())
+	}
+	if err := cursor.Err(); err != nil {
+		return status.Errorf(codes.Internal, fmt.Sprintf("Unkown cursor error: %v", err))
+	}
 	return nil
+}
+
+func (s *UpvoteServiceServer) GetCoinByName(ctx context.Context, req *pb.CoinNameRequest) (*pb.CoinResponse, error) {
+	name := strings.Title(req.GetCoinName())
+	cursor, err := s.CoinsRepository.GetByName(name)
+	if err != nil {
+		return nil, err
+	}
+	var coin models.Coin
+	cursor.Decode(&coin)
+	return coin.ToProtoBuffer(), nil
 }
 
 func (s *UpvoteServiceServer) CreateCoin(ctx context.Context, req *pb.CoinNameRequest) (*pb.CoinResponse, error) {
@@ -34,7 +63,6 @@ func (s *UpvoteServiceServer) CreateCoin(ctx context.Context, req *pb.CoinNameRe
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
 	}
-	// TODO: found, err := s.usersRepository.GetByEmail(req.Email)
 	err := s.CoinsRepository.Create(&coin)
 	if err != nil {
 		return nil, err
@@ -43,5 +71,121 @@ func (s *UpvoteServiceServer) CreateCoin(ctx context.Context, req *pb.CoinNameRe
 	fmt.Print(coin)
 	return coin.ToProtoBuffer(), nil
 
-	// return nil, err
+}
+
+func (s *UpvoteServiceServer) UpdateCoin(ctx context.Context, req *pb.UpdateCoinRequest) (*pb.CoinResponse, error) {
+	objectId, err := bson.ObjectIDFromHex(req.GetId())
+	name := strings.Title(req.GetCoinName())
+	cursor, err := s.CoinsRepository.UpdateCoin(objectId, name)
+	if err != nil {
+		return nil, err
+	}
+	var coin models.Coin
+
+	coin.CoinName = req.CoinName
+	coin.ID = objectId
+	fmt.Println(&cursor)
+	return coin.ToProtoBuffer(), nil
+
+}
+
+func (s *UpvoteServiceServer) RemoveCoin(ctx context.Context, req *pb.CoinIdRequest) (*pb.CoinResponse, error) {
+	objectId, err := bson.ObjectIDFromHex(req.GetId())
+	cursor, err := s.CoinsRepository.DeleteCoin(objectId)
+	if err != nil {
+		return nil, err
+	}
+	var coin models.Coin
+	coin.ID = objectId
+	fmt.Println(&cursor)
+	return coin.ToProtoBuffer(), nil
+
+}
+
+func (s *UpvoteServiceServer) UpVote(ctx context.Context, req *pb.CoinNameRequest) (*pb.CoinResponse, error) {
+	coinName := strings.Title(req.GetCoinName())
+
+	cursor, err := s.CoinsRepository.AddUpvote(coinName)
+	if err != nil {
+		return nil, err
+	}
+
+	if cursor == nil {
+		return nil, status.Errorf(
+			codes.NotFound,
+			fmt.Sprintf("Coin Not found in database %s", req.GetCoinName()),
+		)
+	}
+
+	var coin models.Coin
+	coin.CoinName = coinName
+	fmt.Println(&cursor)
+	return coin.ToProtoBuffer(), nil
+
+}
+
+func (s *UpvoteServiceServer) DownVote(ctx context.Context, req *pb.CoinNameRequest) (*pb.CoinResponse, error) {
+	coinName := strings.Title(req.GetCoinName())
+
+	cursor, err := s.CoinsRepository.AddDownvote(coinName)
+	if err != nil {
+		return nil, err
+	}
+
+	if cursor == nil {
+		return nil, status.Errorf(
+			codes.NotFound,
+			fmt.Sprintf("Coin Not found in database %s", req.GetCoinName()),
+		)
+	}
+
+	var coin models.Coin
+	coin.CoinName = coinName
+	fmt.Println(&cursor)
+	return coin.ToProtoBuffer(), nil
+
+}
+
+func (s *UpvoteServiceServer) RemoveUpVote(ctx context.Context, req *pb.CoinNameRequest) (*pb.CoinResponse, error) {
+	coinName := strings.Title(req.GetCoinName())
+
+	cursor, err := s.CoinsRepository.RemoveUpvote(coinName)
+	if err != nil {
+		return nil, err
+	}
+
+	if cursor == nil {
+		return nil, status.Errorf(
+			codes.NotFound,
+			fmt.Sprintf("Coin Not found in database %s", req.GetCoinName()),
+		)
+	}
+
+	var coin models.Coin
+	coin.CoinName = coinName
+	fmt.Println(&cursor)
+	return coin.ToProtoBuffer(), nil
+
+}
+
+func (s *UpvoteServiceServer) RemoveDownvote(ctx context.Context, req *pb.CoinNameRequest) (*pb.CoinResponse, error) {
+	coinName := strings.Title(req.GetCoinName())
+
+	cursor, err := s.CoinsRepository.RemoveDownvote(coinName)
+	if err != nil {
+		return nil, err
+	}
+
+	if cursor == nil {
+		return nil, status.Errorf(
+			codes.NotFound,
+			fmt.Sprintf("Coin Not found in database %s", req.GetCoinName()),
+		)
+	}
+
+	var coin models.Coin
+	coin.CoinName = coinName
+	fmt.Println(&cursor)
+	return coin.ToProtoBuffer(), nil
+
 }
