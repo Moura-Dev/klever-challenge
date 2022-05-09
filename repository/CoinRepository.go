0320aa829a2ab2584786b8549c48764dd53a0f00
moura-dev <junior.moura19@hotmail.com>
2022-05-08 22:01:48 -0300

    Implement List coins and create coins in repository

--- repository/CoinRepository.go
+++ repository/CoinRepository.go
@@ -3,103 +3,43 @@ package repository
 import (
 	"context"
 	"fmt"
+	"klever-challenge/db"
 	"klever-challenge/models"
 
-	bson "go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo/options"
-
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type CoinsRepositoryInterface interface {
-	GetAll() (mongo.Cursor, error)
-	GetById(id string) (*models.Coin, error)
-	GetByName(name string) (*models.Coin, error)
-	Create(coin *models.Coin) error
-	Update(coin *models.Coin) error
-	DropCollection() error
-	Delete(id string) error
+const coinsCollection = "coins"
+
+type CoinsRepository interface {
+	GetAll() (*mongo.Cursor, error)
+	Create(*models.Coin) error
 }
 
-type CoinsRepository struct {
-	Db  *mongo.Collection
-	Ctx context.Context
+type coinRepository struct {
+	db *mongo.Collection
 }
 
-// GetAll implements CoinsRepository
-func (r *CoinsRepository) GetAll() (cur mongo.Cursor, err error) {
-	cursor, err := r.Db.Find(r.Ctx, bson.M{}, options.Find().SetSort(bson.M{}))
-	if err != nil {
-		return cur, status.Errorf(codes.Internal, fmt.Sprintf("Not found %v", err))
-	}
-	defer cursor.Close(r.Ctx)
-
-	return cur, nil
+func NewCoinRepository(conn db.Connection) CoinsRepository {
+	return &coinRepository{db: conn.DB().Collection(coinsCollection)}
 }
 
-// GetByName implements CoinsRepository
-func (r *CoinsRepository) GetByName(name string) (coin *models.Coin, err error) {
-	cursor, err := r.Db.Find(r.Ctx, bson.M{"name": name}, options.Find().SetSort(bson.M{}))
+func (r *coinRepository) GetAll() (curso *mongo.Cursor, err error) {
+	// cur, err := r.db.Find(nil, nil)
+	cursor, err := r.db.Find(context.Background(), bson.M{}, options.Find().SetSort(bson.M{}))
+
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, fmt.Sprintf("Not found %v", err))
+		return nil, err
 	}
-	cursor.All(r.Ctx, &coin)
-	return coin, nil
+	fmt.Println(cursor)
+	return curso, nil
 }
 
-// GetById implements CoinsRepository
-func (r *CoinsRepository) GetById(id string) (coin *models.Coin, err error) {
-	objectId, err := bson.ObjectIDFromHex(id)
-	if err != nil {
-		return nil, status.Errorf(codes.InvalidArgument, fmt.Sprintf("Invalid id: %v", err))
-	}
-	cursor, err := r.Db.Find(r.Ctx, bson.M{"_id": objectId}, options.Find().SetSort(bson.M{}))
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, fmt.Sprintf("Not found %v", err))
-	}
-	cursor.All(r.Ctx, &coin)
-	return coin, nil
-}
+func (r *coinRepository) Create(coin *models.Coin) error {
 
-// CreateCoin implements CoinsRepository
-func (r *CoinsRepository) Create(coin *models.Coin) error {
-	_, err := r.Db.InsertOne(r.Ctx, []interface{}{coin})
-
-	if err != nil {
-		return status.Errorf(codes.Internal, fmt.Sprintf("Insert Failed error: %v", err))
-	}
+	cursor, err := r.db.InsertOne(context.Background(), coin)
+	fmt.Println(cursor, err)
 	return nil
-}
-
-// Update implements CoinsRepository
-func (r *CoinsRepository) Update(coin *models.Coin) error {
-
-	_, err := r.Db.UpdateByID(r.Ctx, coin.ID, coin)
-	if err != nil {
-		return status.Errorf(codes.Internal, fmt.Sprintf("Update Failed error: %v", err))
-	}
-	return nil
-}
-
-func (r *CoinsRepository) DropCollection() error {
-	err := r.Db.Drop(r.Ctx)
-	if err != nil {
-		return status.Errorf(codes.Internal, fmt.Sprintf("Drop Collection Failed error: %v", err))
-	}
-	return nil
-}
-
-func (r *CoinsRepository) Delete(id string) error {
-	objectId, err := bson.ObjectIDFromHex(id)
-	if err != nil {
-		return status.Errorf(codes.InvalidArgument, fmt.Sprintf("Invalid id: %v", err))
-
-	}
-	cursor, err := r.Db.DeleteOne(r.Ctx, bson.M{"_id": objectId})
-	if err != nil {
-		return status.Errorf(codes.Internal, fmt.Sprintf("Delete Failed error: %v", err))
-	}
-	return cursor
 }
